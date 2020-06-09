09866ab6bcec9d14f8983704badad1f9ffc025fc
umairsadiq88 <umairsadiq2010@gmail.com>
2020-06-09 16:00:43 +0500

    update app.js with adding login n info with using props argument

--- assign_1/src/App.js
+++ assign_1/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import './App.css';
-import Login from './components/Login/Login';
+import React from "react";
+import "./App.css";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div>
-      <h1>hello World!</h1>
-      <Login /> 
+      <h1>My First Bootcamp Assignment!</h1>
+      <Login name="Bilal" age="40" hobby="Coding" />
+      <hr />
+      <Login name="Umair" age="32" hobby="watching Ertugal" />
     </div>
-
   );
 }
 
--- assign_1/src/components/Info.js
+++ assign_1/src/components/Info.js
@@ -1,22 +1,19 @@
 import React from "react";
-import Info from "../Info";
-function Login(props) {
+
+function Info(props) {
   console.log(props);
 
   return (
     <div>
-      <h3>Login Form</h3>
+      <h3>Info Page</h3>
       <p>
         {" "}
         My Name is {props.name}. <br />
         My Age is {props.age} <br />
         My hobby is {props.hobby}.{" "}
       </p>
-      <hr />
-      <Info name={props.name} age={props.age} hobby={props.hobby} />
-      
     </div>
   );
 }
 
-export default Login;
+export default Info;
